4c5969e6e839297a348a71bf9d20c0607aee6cdd
Muhammed Asaad <muhammed.asaad75@egmaile.com>
2023-08-18 10:45:41 +0300

    Set up the Migrations and Models

---
+++ app/Models/BarcodeGeneration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarcodeGeneration extends Model
+{
+    use HasFactory;
+
+    protected $table = 'barcode_generations';
+
+}
---
+++ database/migrations/2023_08_18_072931_create_barcode_generation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barcode_generations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->string('barcode');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barcode_generation');
+    }
+};
